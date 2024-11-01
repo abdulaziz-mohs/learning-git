9db650db08e5d05011917d0de1897e5295cc8a2d
abdulaziz <accoabdulazizmohammed@gmail.com>
2024-11-01 16:52:54 +0300

    second commit

--- check.cpp
+++ check.cpp
@@ -5,14 +5,17 @@ int main() {
     int count = 0; // Variable to keep track of the total number of iterations
 
     // Outer loop (10 iterations)
-    for (int i = 1; i <= 1000; ++i) {
+    for (int i = 1; i <= 10; ++i) {
         // Inner loop (10 iterations for each outer iteration)
         for (int j = 1; j <= 10; ++j) {
             count++; // Increment the count for each iteration
             cout << "Iteration " << count << ": Outer loop (i = " << i << "), Inner loop (j = " << j << ")" << endl;
         }
     }
-
     cout << "Total iterations: " << count << endl;
+    
+    for (int i=0;i<10;i++){
+        cout<<"hello"<<endl;
+    }
     return 0;
 }
